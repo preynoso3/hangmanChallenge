347ab9aaf61f483eea07963b9e752aca6cca360f
Pedro Reynoso <113930313+preynoso3@users.noreply.github.com>
2022-10-27 17:53:26 +0000

    #   Return Practice #2

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # from methods_help import method_help
-from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers, greet, square
-# from returnStuff import returnS
+# from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers, greet, square
+from returnStuff import power, usd_eur
 
 # method_help()
-function()
-welcome("")
-added = add_numbers(4,5)
-print(added)
+# function()
+# welcome("")
+# added = add_numbers(4,5)
+# print(added)
 
-final = num_dividedFrom_add_numbers(45, added)
-print(final)
-
-sqr=square(7)
-print(sqr)
+# final = num_dividedFrom_add_numbers(45, added)
+# print(final)
+# returnS()
+powers=power(4,56)
+print(powers)
+eur= usd_eur(2436)
+print(eur)
+# sqr=square(7)
+# print(sqr)
 # returnS()
